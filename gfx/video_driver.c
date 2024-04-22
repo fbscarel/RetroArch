73f36c68e6a4b07d3c53c1c79be72e2e97065f1b
Antonio Giner <calamity15khz@gmail.com>
2024-04-22 18:20:04 +0200

    Disable super resolutions for MiSTer.

--- gfx/video_driver.c
+++ gfx/video_driver.c
@@ -4406,6 +4406,10 @@ void video_driver_frame(const void *data, unsigned width,
 
          video_st->flags          |= VIDEO_FLAG_CRT_SWITCHING_ACTIVE;
 
+         // MiSTer doesn't support super resolutions
+         if (config_get_ptr()->bools.video_mister_enable && video_info.crt_switch_resolution_super)
+            video_info.crt_switch_resolution_super = 0;
+
          switch (video_info.crt_switch_resolution_super)
          {
             case 2560:
